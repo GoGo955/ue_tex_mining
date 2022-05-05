7e914cab6a311711d76448a7c3ea8697311555c2
GoGo955 <77108321+GoGo955@users.noreply.github.com>
2022-05-05 08:53:06 +0200

    load and decode data

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
+from utils import (
+    load_data,
+    decode_data
+)
+
+
 if __name__ == "__main__":
-    print('hello')
+    raw_df = load_data("compare_matches.xlsx")
+    preprocessed_df = decode_data(raw_df)
---
+++ utils.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+
+def load_data(filename: str) -> pd.DataFrame:
+    """
+    Reads the xlsx file from data folder for a given filename
+    Returns raw DataFrame.
+    """
+    return pd.read_excel(
+        f"data\{filename}",
+        header=0,
+    )
+
+
+def decode_data(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Normalizes the text to utf-8, removes accents etc.
+    Returns cleaned df.
+    """
+    cols = df.select_dtypes(include=[object]).columns
+    return (
+        df[cols]
+        .apply(
+            lambda x:
+            x.str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8')
+        )
+    )
