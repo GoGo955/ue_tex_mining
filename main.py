69cbe7423156063c59e3da60a3225343eb9f9d53
GoGo955 <77108321+GoGo955@users.noreply.github.com>
2022-05-06 21:08:12 +0200

    solution, dirty state

--- main.py
+++ main.py
@@ -1,26 +1,33 @@
+from warnings import simplefilter
 import pandas as pd
 
 from utils import (
     load_data,
     decode_data,
-    clean_name_cols
+    clean_name_cols,
+    extract_simple_matches,
+    find_best_match,
+    cartesian_product_simplified
 )
 
 
-def check_shape():
-    # temp shape check
-    print(f"Raw df row count: {raw_df.shape[0]}")
-    print(f"Clean df row count: {clean_df.shape[0]}")
-    print(f"Duplicate df row count: {duplicates_df.shape[0]}")
-    print(
-        "Summary row count after first (simple) phase of matching:",
-        f"{substr_match_df.shape[0] + exact_match_df.shape[0] + unmatched_df.shape[0]}"
-    )
-
-
-if __name__ == "__main__":
+def execute():
+    def check_shape():
+        # temp shape check
+        print(f"Raw df row count: {raw_df.shape[0]}")
+        print(f"Clean df row count: {clean_df.shape[0]}")
+        print(f"Duplicate df row count: {duplicates_df.shape[0]}")
+        print(
+            "Summary row count after first (simple) phase of matching:",
+            f"{substr_match_df.shape[0] + exact_match_df.shape[0] + unmatched_df.shape[0]}"
+        )
+        print(f"Exact match df row count: {exact_match_df.shape[0]}")
+        print(f"Substrung match df row count: {substr_match_df.shape[0]}")
+        print(f"Unmatched df row count: {unmatched_df.shape[0]}")
+    # suppress pandas future warning
+    simplefilter(action='ignore', category=FutureWarning)
     # raw
-    raw_df = load_data("compare_matches.xlsx")
+    raw_df = load_data("compare_matches.xlsx", 500)
 
     # preprocessed
     preprocessed_df = decode_data(raw_df)
@@ -35,25 +42,32 @@ if __name__ == "__main__":
     )
 
     # phase 1 - simple matching
-    exact_match_df = clean_df[clean_df['name_1_clean'] == clean_df['name_2_clean']]
-
-    substr_match_df = pd.concat([clean_df, exact_match_df]).drop_duplicates(keep=False)
-    substr_match_df['is_substr'] = [
-        row[0] in row[1] or row[1] in row[0]
-        for row
-        in zip(substr_match_df['name_1_clean'], substr_match_df['name_2_clean'])
-    ]
-    # figure out if this is correct - if not, remove E712 from setup cfg
-    substr_match_df = substr_match_df[substr_match_df['is_substr'] == True]
-
-    unmatched_df = (
-        pd
-        .concat([clean_df, exact_match_df, substr_match_df.drop('is_substr', axis=1)])
-        .drop_duplicates(keep=False)
-    )
-
+    exact_match_df, substr_match_df, unmatched_df = extract_simple_matches(clean_df)
     check_shape()
 
-    # phase 2 - similarities
+    # phase 2 - similarities. split unmatched to two sample dfs, create a cartesian product
+    # and apply the lev dist func.
+    left = unmatched_df['name_1_clean']   # use .head(1000) on full df
+    right = unmatched_df['name_2_clean']
+    # use code below for full df
+    # right = pd.concat([
+    #     unmatched_df['name_2_clean'].head(500),
+    #     unmatched_df['name_2_clean'].sample(500)]
+    # )
+    cart_df = cartesian_product_simplified(left, right)
+    cart_df['lev'] = cart_df.apply(
+        lambda row: find_best_match(row['name_1_clean'], row['name_2_clean']),
+        axis=1
+    )
+    cart_df['min_lev'] = cart_df.groupby('name_1_clean')['lev'].rank(method='min')
 
-    # preprocessed_df.to_excel('data\preprocessed_matches.xlsx')
+    results_df = cart_df[cart_df['min_lev'].isin(range(4))].sort_values(['name_1_clean', 'min_lev'])
+    print(results_df.head(20))
+    # # cart_df.to_excel('data\cart_prod_sample.xlsx', engine='openpyxl')
+    # # clean_df.to_excel('data\clean_matches.xlsx', engine='openpyxl')
+
+
+if __name__ == "__main__":
+    execute()
+    # df = load_data('cart_prod_sample.xlsx').drop('Unnamed: 0', axis=1)
+    # print(df.head(10))
--- utils.py
+++ utils.py
@@ -1,16 +1,19 @@
-from tkinter.messagebox import RETRY
 import pandas as pd
-import re
+import itertools as it
+from Levenshtein import distance as lev
+import math
+import numpy as np
 
 
-def load_data(filename: str) -> pd.DataFrame:
+def load_data(filename: str, rows: int) -> pd.DataFrame:
     """
-    Reads the xlsx file from data folder for a given filename
+    Reads the 'n' rows of xlsx file from data folder for a given filename
     Returns raw DataFrame.
     """
     return pd.read_excel(
         f"data\{filename}",
         header=0,
+        nrows=rows
     )
 
 
@@ -37,5 +40,91 @@ def clean_name_cols(df: pd.DataFrame, cols: list([str])) -> pd.DataFrame:
     for colname in cols:
         df[f'{colname}_clean'] = df[colname].str.lower()
         df[f'{colname}_clean'] = df[f'{colname}_clean'].str.replace("[^A-Za-z\s\d]+", "")
-        df[f'{colname}_clean'] = df[f'{colname}_clean'].str.replace("\s+", "")
+        df[f'{colname}_clean'] = df[f'{colname}_clean'].str.replace("\s{2,}", " ")
+        df[f'{colname}_clean_1'] = df[f'{colname}_clean'].str.replace("\s+", "")
     return df
+
+
+def extract_simple_matches(clean_df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Transforms input cleaned DF as three separate DataFrames. First one contains exact matches,
+    second contains matches based on substrings (col1 is substr of col2 or vice versa) and the
+    third one contains the rest - all unmatched rows (in this simple manner).
+    Returns: (exact_match_df, substr_match_df, unmatched_df)
+    """
+    exact_match_df = clean_df[clean_df['name_1_clean'] == clean_df['name_2_clean']]
+
+    substr_match_df = pd.concat([clean_df, exact_match_df]).drop_duplicates(keep=False)
+    substr_match_df['is_substr'] = [
+        row[0] in row[1] or row[1] in row[0]
+        for row
+        in zip(substr_match_df['name_1_clean'], substr_match_df['name_2_clean'])
+    ]
+    # figure out if this is correct - if not, remove E712 from setup cfg
+    substr_match_df = substr_match_df[substr_match_df['is_substr'] == True]
+
+    unmatched_df = (
+        pd
+        .concat([clean_df, exact_match_df, substr_match_df.drop('is_substr', axis=1)])
+        .drop_duplicates(keep=False)
+    )
+    return exact_match_df, substr_match_df, unmatched_df
+
+
+def convert_tuple_to_string(some_tuple, join_type=' '):
+    '''Convert tuple to string'''
+    string_from_tuple = [join_type.join(item) for item in some_tuple]
+    return string_from_tuple
+
+
+def find_inner_permututations(team_name_):
+    '''Find permutations of different possible length'''
+    team_name = team_name_.split(" ")
+    team_name_permutations = [
+        x for len in range(1, len(team_name) + 1) for x in it.combinations(team_name, len)
+    ]
+    perm_list_string = convert_tuple_to_string(team_name_permutations)
+    return perm_list_string
+
+
+def find_best_match(col_a, col_b, threshold=5):
+
+    team_a = find_inner_permututations(col_a)
+    team_b = find_inner_permututations(col_b)
+    permutations = []
+
+    # get cartesian product
+    for combination in it.product(team_a, team_b):
+        permutations.append(combination)
+
+    # Combination dict: key - tuple with team_name permutations, value - score based on levenshtein,
+    #                   match total length and intersection including char exact ordering
+    actual_score_dict = {perm_tuple: math.inf for perm_tuple in permutations}
+
+    # length of the shorter team name to avoid null id matching during intersection
+    mini = min(len(ele) for ele in [col_a, col_b])
+
+    # intersection involves matching chars on full team names and NOT their permutations
+    intersection = len(list(set(col_a[:mini]).intersection(col_b[:mini]))) + 1
+
+    for perm_tuple in actual_score_dict:
+        lev_score = lev(perm_tuple[0], perm_tuple[1]) + 1
+        match_len = len(perm_tuple[0] + perm_tuple[1]) + 1
+
+        if match_len > threshold:
+            actual_score_dict[perm_tuple] = (lev_score) * (1 / (match_len * intersection))
+
+    # get the best score
+    min_score = min(actual_score_dict, key=actual_score_dict.get)
+
+    return actual_score_dict[min_score]
+
+
+def cartesian_product_simplified(left, right):
+    la, lb = len(left), len(right)
+    ia2, ib2 = np.broadcast_arrays(*np.ogrid[:la, :lb])
+
+    return pd.DataFrame(
+        np.column_stack([left.values[ia2.ravel()], right.values[ib2.ravel()]]),
+        columns=['name_1_clean', 'name_2_clean']
+    )
